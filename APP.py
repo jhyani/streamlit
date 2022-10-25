5a1bf6c51263c99caac109a4ad9685eb9c783502
yjh4350 <yjh4350@163.com>
2022-10-25 22:14:36 +0800

    change title

--- APP.py
+++ APP.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 st.set_page_config(
-    page_title="Ex-stream-ly Cool App",
+    page_title="YoungYan",
     page_icon="🧊",
     layout="wide",
     initial_sidebar_state="expanded",
