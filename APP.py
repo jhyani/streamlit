67ee5a157b4e47bd45fe6644eb5e3a18e8b642b0
jhyani <77726337+jhyani@users.noreply.github.com>
2022-10-27 23:13:09 +0800

    Update APP.py

--- APP.py
+++ APP.py
@@ -20,5 +20,30 @@ st.write("my first web")
 
 
 # Everything is accessible via the st.secrets dict:
+import mysql.connector
+server = "rm-uf6k846wp4uld2q73po.mysql.rds.aliyuncs.com"
+users = "jhyani"
+pword = "Yjh950125"
+database = "nongji"
 
-st.write("host:", st.secrets["host"])
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return mysql.connector.connect(host=server,port=3306,database="nongji",user="jhyani",password="Yjh950125")
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from test1;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
